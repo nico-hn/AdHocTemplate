ff94e112376dbf55a20abe0e765f92eabcc5599c
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-02-26 08:06:38 +0900

    added a test for AdHocParser::Parser.parse()

--- spec/ad_hoc_template_spec.rb
+++ spec/ad_hoc_template_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
+require 'ad_hoc_template'
 
 describe AdHocTemplate do
   it 'should have a version number' do
     AdHocTemplate::VERSION.should_not be_nil
+  end
+
+  describe AdHocTemplate::Parser do
+    it "returns a tree of TagNode and Leaf" do
+      expect(AdHocTemplate::Parser.parse("a test string with tags (<% the first tag %> and <% the second tag %>) in it")).to eq([["a test string with tags ("],
+                                                                                                                                 [[" the first tag "]],
+                                                                                                                                 [" and "],
+                                                                                                                                 [[" the second tag "]],
+                                                                                                                                 [") in it"]])
+    end
   end
 end
