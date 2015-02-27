240385e0f26dd9ff84bdce6ae44a9568f02adc64
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-02-28 07:48:00 +0900

    added a test for ConfigReader

--- spec/ad_hoc_template_spec.rb
+++ spec/ad_hoc_template_spec.rb
@@ -22,4 +22,36 @@ describe AdHocTemplate do
                                                                                                                                  [" "]]])
     end
   end
+
+  describe AdHocTemplate::ConfigReader do
+    it "reads configuration data and turns them into a hash object" do
+      data = <<CONFIG
+key1: value1
+key2: value2
+key3: value3
+
+//@block1
+
+the first line of block1
+the second line of block1
+
+the second paragraph in block1
+
+//@block2
+the first line of block2
+the second line of block2
+
+the second paragraph of block2
+CONFIG
+
+expected_config = {
+        "key1" => "value1",
+        "key2" => "value2",
+        "key3" => "value3",
+        "block1" => "the first line of block1\nthe second line of block1\n\nthe second paragraph in block1\n",
+        "block2" => "the first line of block2\nthe second line of block2\n\nthe second paragraph of block2\n"
+      }
+      expect(AdHocTemplate::ConfigReader.read_config(data)).to eq(expected_config)
+    end
+  end
 end
