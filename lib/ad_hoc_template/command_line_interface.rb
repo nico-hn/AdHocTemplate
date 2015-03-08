6da3caefb6c9c8e6e5ddf982c402cbb2374957ca
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-03-08 23:02:51 +0900

    added CommandLineInterface#parse_command_line_options()

--- lib/ad_hoc_template/command_line_interface.rb
+++ lib/ad_hoc_template/command_line_interface.rb
@@ -10,5 +10,16 @@ module AdHocTemplate
       Encoding.default_external = external if external and not external.empty?
       Encoding.default_internal = internal if internal and not internal.empty?
     end
+
+    def parse_command_line_options
+      OptionParser.new do |opt|
+        opt.on("-E [ex[:in]]", "--encoding [=ex[:in]]",
+               "Specify the default external and internal character encodings (same as the option of MRI") do |given_opt|
+          self.set_encoding(given_opt)
+        end
+
+        opt.parse!
+      end
+    end
   end
 end
