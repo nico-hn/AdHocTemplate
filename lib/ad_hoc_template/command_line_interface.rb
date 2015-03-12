0be8eb70ae5fe8429e1d331290aab8ad3db9f5dd
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-03-13 02:44:57 +0900

    added a command line option "-o" that specifies an output file

--- lib/ad_hoc_template/command_line_interface.rb
+++ lib/ad_hoc_template/command_line_interface.rb
@@ -5,6 +5,12 @@ require 'optparse'
 
 module AdHocTemplate
   class CommandLineInterface
+    attr_accessor :output_filename
+
+    def initialize
+      @output_filename = nil
+    end
+
     def set_encoding(given_opt)
       external, internal = given_opt.split(/:/o, 2)
       Encoding.default_external = external if external and not external.empty?
@@ -18,7 +24,12 @@ module AdHocTemplate
           self.set_encoding(given_opt)
         end
 
-        opt.parse!
+        opt.on("-o [output_file]", "--output [=output_file]",
+               "Save the result into the specified file.") do |output_file|
+          @output_filename = File.expand_path(output_file)
+        end
+
+       opt.parse!
       end
     end
   end
