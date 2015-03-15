d79b97b51b621357fc6c6645f123e435a77f2b9d
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-03-16 00:34:29 +0900

    added a test for iteration blocks without key label

--- spec/ad_hoc_template_spec.rb
+++ spec/ad_hoc_template_spec.rb
@@ -188,6 +188,50 @@ RESULT
       expect(AdHocTemplate::Converter.new(config).format(tree)).to eq(expected_result)
     end
 
+    it "may contains iteration blocks without key label." do
+      template = <<TEMPLATE
+a test string with tags (<%= key1 %> and <%= key2 %>) in it
+
+<%#
+the value of key1 is <%= key1 %>
+the value of key2 is <%= key2 %>
+
+#%>
+<%= block %>
+TEMPLATE
+
+      config_data = <<CONFIG
+key1: value1
+key2: value2
+key3: value3
+
+//@block
+
+the first line of block
+the second line of block
+
+the second paragraph in block
+
+CONFIG
+
+expected_result = <<RESULT
+a test string with tags (value1 and value2) in it
+
+the value of key1 is value1
+the value of key2 is value2
+
+
+the first line of block
+the second line of block
+
+the second paragraph in block
+
+RESULT
+      tree = AdHocTemplate::Parser.parse(template)
+      config = AdHocTemplate::RecordReader.read_record(config_data)
+      expect(AdHocTemplate::Converter.new(config).format(tree)).to eq(expected_result)
+    end
+
     it 'can convert &"<> into character entities' do
       result = AdHocTemplate::Converter.convert('characters: &, ", < and >',
                                        'a string with characters (<%h characters %>) that should be represented as character entities.')
