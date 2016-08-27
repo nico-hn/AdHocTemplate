622686dc1f4acf6ffe439bd16faf161c34e3c383
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-08-27 09:49:11 +0900

    add tests for Parser with square brakets tag type

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -45,5 +45,38 @@ content
         expect(tree).to eq([])
       end
     end
+
+    describe "with the square brackets tag type" do
+      it "returns a tree of TagNode and Leaf" do
+        expect(AdHocTemplate::Parser.parse("a test string with tags ([[ the first tag ]] and [[ the second tag ]]) in it", :square_brackets)).to eq([["a test string with tags ("],
+                                                                                                                                    [[" the first tag "]],
+                                                                                                                                    [" and "],
+                                                                                                                                    [[" the second tag "]],
+                                                                                                                                    [") in it"]])
+      end
+
+      it "allows to have a nested tag" do
+        expect(AdHocTemplate::Parser.parse("a test string with a nested tag; [[ an outer tag and [[ an inner tag ]] ]]", :square_brackets)).to eq([["a test string with a nested tag; "],
+                                                                                                                                  [[" an outer tag and "],
+                                                                                                                                    [[" an inner tag "]],
+                                                                                                                                    [" "]]])
+      end
+
+      it "may have iteration tags." do
+        tree = AdHocTemplate::Parser.parse("a test string with a nested tag: [[# an iteration tag and [[ an inner tag ]] #]] and [[ another tag ]]", :square_brackets)
+        expect(tree).to eq([["a test string with a nested tag: "],
+                             [[" an iteration tag and "],
+                               [[" an inner tag "]],
+                               [" "]],
+                             [" and "],
+                             [[" another tag "]]])
+        expect(tree[1]).to be_a_kind_of(AdHocTemplate::Parser::IterationTagNode)
+      end
+
+      it "must return an empty array when template is an empty string" do
+        tree = AdHocTemplate::Parser.parse("")
+        expect(tree).to eq([])
+      end
+    end
   end
 end
