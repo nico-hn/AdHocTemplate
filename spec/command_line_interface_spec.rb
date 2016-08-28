104a13a7f93800fe73cd2c791e45cf61f79bddef
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-08-28 13:26:18 +0900

    define before hook in the example group for CommandLineInterface

--- spec/command_line_interface_spec.rb
+++ spec/command_line_interface_spec.rb
@@ -8,6 +8,57 @@ require 'ad_hoc_template/command_line_interface'
 
 describe AdHocTemplate do
   describe AdHocTemplate::CommandLineInterface do
+    before do
+      @template_in_default_format = <<TEMPLATE
+a test string with tags (<%= key1 %> and <%= key2 %>) in it
+
+<%#iteration_block
+the value of sub_key1 is <%= sub_key1 %>
+the value of sub_key2 is <%= sub_key2 %>
+
+#%>
+<%= block %>
+TEMPLATE
+
+      @record_in_default_format = <<CONFIG
+key1: value1
+key2: value2
+key3: value3
+
+//@#iteration_block
+
+sub_key1: value1-1
+sub_key2: value1-2
+
+sub_key1: value2-1
+sub_key2: value2-2
+
+//@block
+
+the first line of block
+the second line of block
+
+the second paragraph in block
+
+CONFIG
+
+      @expected_result = <<RESULT
+a test string with tags (value1 and value2) in it
+
+the value of sub_key1 is value1-1
+the value of sub_key2 is value1-2
+
+the value of sub_key1 is value2-1
+the value of sub_key2 is value2-2
+
+the first line of block
+the second line of block
+
+the second paragraph in block
+
+RESULT
+    end
+
     it "can set the input/output encoding" do
       command_line_interface = AdHocTemplate::CommandLineInterface.new
       command_line_interface.set_encoding("UTF-8:Shift_JIS")
@@ -95,59 +146,9 @@ describe AdHocTemplate do
       template_filename = "template.txt"
       record_filename = "record.txt"
 
-      template = <<TEMPLATE
-a test string with tags (<%= key1 %> and <%= key2 %>) in it
-
-<%#iteration_block
-the value of sub_key1 is <%= sub_key1 %>
-the value of sub_key2 is <%= sub_key2 %>
-
-#%>
-<%= block %>
-TEMPLATE
-
-      record = <<CONFIG
-key1: value1
-key2: value2
-key3: value3
-
-//@#iteration_block
-
-sub_key1: value1-1
-sub_key2: value1-2
-
-sub_key1: value2-1
-sub_key2: value2-2
-
-//@block
-
-the first line of block
-the second line of block
-
-the second paragraph in block
-
-CONFIG
-
-      expected_result = <<RESULT
-a test string with tags (value1 and value2) in it
-
-the value of sub_key1 is value1-1
-the value of sub_key2 is value1-2
-
-the value of sub_key1 is value2-1
-the value of sub_key2 is value2-2
-
-the first line of block
-the second line of block
-
-the second paragraph in block
-
-RESULT
-
-
-      allow(File).to receive(:read).with(File.expand_path(template_filename)).and_return(template)
-      allow(File).to receive(:read).with(File.expand_path(record_filename)).and_return(record)
-      allow(STDOUT).to receive(:print).with(expected_result)
+      allow(File).to receive(:read).with(File.expand_path(template_filename)).and_return(@template_in_default_format)
+      allow(File).to receive(:read).with(File.expand_path(record_filename)).and_return(@record_in_default_format)
+      allow(STDOUT).to receive(:print).with(@expected_result)
 
       set_argv("#{template_filename} #{record_filename}")
       command_line_interface = AdHocTemplate::CommandLineInterface.new
@@ -169,48 +170,9 @@ the value of sub_key2 is {{= sub_key2 }}
 {{= block }}
 TEMPLATE
 
-      record = <<CONFIG
-key1: value1
-key2: value2
-key3: value3
-
-//@#iteration_block
-
-sub_key1: value1-1
-sub_key2: value1-2
-
-sub_key1: value2-1
-sub_key2: value2-2
-
-//@block
-
-the first line of block
-the second line of block
-
-the second paragraph in block
-
-CONFIG
-
-      expected_result = <<RESULT
-a test string with tags (value1 and value2) in it
-
-the value of sub_key1 is value1-1
-the value of sub_key2 is value1-2
-
-the value of sub_key1 is value2-1
-the value of sub_key2 is value2-2
-
-the first line of block
-the second line of block
-
-the second paragraph in block
-
-RESULT
-
-
       allow(File).to receive(:read).with(File.expand_path(template_filename)).and_return(template)
-      allow(File).to receive(:read).with(File.expand_path(record_filename)).and_return(record)
-      allow(STDOUT).to receive(:print).with(expected_result)
+      allow(File).to receive(:read).with(File.expand_path(record_filename)).and_return(@record_in_default_format)
+      allow(STDOUT).to receive(:print).with(@expected_result)
 
       set_argv("--tag-type=curly_brackets #{template_filename} #{record_filename}")
       command_line_interface = AdHocTemplate::CommandLineInterface.new
