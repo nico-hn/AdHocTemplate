5d595e38638cd27a06484fa5bf5f6eaa259385ce
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-08-28 14:33:37 +0900

    update the help message

--- lib/ad_hoc_template/command_line_interface.rb
+++ lib/ad_hoc_template/command_line_interface.rb
@@ -33,7 +33,7 @@ module AdHocTemplate
     end
 
     def parse_command_line_options
-      OptionParser.new do |opt|
+      OptionParser.new("USAGE: #{File.basename($0)} [OPTION]... TEMPLATE_FILE DATA_FILE") do |opt|
         opt.on("-E [ex[:in]]", "--encoding [=ex[:in]]",
                "Specify the default external and internal character encodings (same as the option of MRI") do |given_opt|
           self.set_encoding(given_opt)
