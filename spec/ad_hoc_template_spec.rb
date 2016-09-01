e8633a3330842b1dc51420b23cd91f07fb70ef4a
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-09-01 16:28:10 +0900

    in the example group of tests for DataLoader, move common data into the before hook

--- spec/ad_hoc_template_spec.rb
+++ spec/ad_hoc_template_spec.rb
@@ -2,25 +2,14 @@ require 'spec_helper'
 require 'ad_hoc_template'
 
 describe AdHocTemplate do
+
   it 'should have a version number' do
     expect(AdHocTemplate::VERSION).to_not be_nil
   end
 
   describe AdHocTemplate::DataLoader do
-    it "returns the result of conversion." do
-      template = "a test string with tags (<%= key1 %> and <%= key2 %>) in it"
-      config_data = <<CONFIG
-key1: value1
-key2: value2
-CONFIG
-
-      tree = AdHocTemplate::Parser.parse(template)
-      config = AdHocTemplate::RecordReader.read_record(config_data)
-      expect(AdHocTemplate::DataLoader.new(config).format(tree)).to eq("a test string with tags (value1 and value2) in it")
-    end
-
-    it "accepts a template with an iteration block and evaluate repeatedly the block" do
-      template = <<TEMPLATE
+    before do
+      @template_with_an_iteration_block = <<TEMPLATE
 a test string with tags (<%= key1 %> and <%= key2 %>) in it
 
 <%#iteration_block
@@ -31,7 +20,7 @@ the value of sub_key2 is <%= sub_key2 %>
 <%= block %>
 TEMPLATE
 
-      config_data = <<CONFIG
+      @config_data_with_an_iteration_block = <<CONFIG
 key1: value1
 key2: value2
 key3: value3
@@ -53,7 +42,7 @@ the second paragraph in block
 
 CONFIG
 
-expected_result = <<RESULT
+      @expected_result_with_an_iteration_block = <<RESULT
 a test string with tags (value1 and value2) in it
 
 the value of sub_key1 is value1-1
@@ -68,6 +57,25 @@ the second line of block
 the second paragraph in block
 
 RESULT
+    end
+
+    it "returns the result of conversion." do
+      template = "a test string with tags (<%= key1 %> and <%= key2 %>) in it"
+      config_data = <<CONFIG
+key1: value1
+key2: value2
+CONFIG
+
+      tree = AdHocTemplate::Parser.parse(template)
+      config = AdHocTemplate::RecordReader.read_record(config_data)
+      expect(AdHocTemplate::DataLoader.new(config).format(tree)).to eq("a test string with tags (value1 and value2) in it")
+    end
+
+    it "accepts a template with an iteration block and evaluate repeatedly the block" do
+      template = @template_with_an_iteration_block
+      config_data = @config_data_with_an_iteration_block
+      expected_result = @expected_result_with_an_iteration_block
+
       tree = AdHocTemplate::Parser.parse(template)
       config = AdHocTemplate::RecordReader.read_record(config_data)
       expect(AdHocTemplate::DataLoader.new(config).format(tree)).to eq(expected_result)
@@ -117,60 +125,17 @@ the second line of block
 the second paragraph in block
 
 RESULT
+
       tree = AdHocTemplate::Parser.parse(template)
       config = AdHocTemplate::RecordReader.read_record(config_data)
       expect(AdHocTemplate::DataLoader.new(config).format(tree)).to eq(expected_result)
     end
 
     it "offers .format method for convenience" do
-      template = <<TEMPLATE
-a test string with tags (<%= key1 %> and <%= key2 %>) in it
+      template = @template_with_an_iteration_block
+      config_data = @config_data_with_an_iteration_block
+      expected_result = @expected_result_with_an_iteration_block
 
-<%#iteration_block
-the value of sub_key1 is <%= sub_key1 %>
-the value of sub_key2 is <%= sub_key2 %>
-
-#%>
-<%= block %>
-TEMPLATE
-
-      config_data = <<CONFIG
-key1: value1
-key2: value2
-key3: value3
-
-//@#iteration_block
-
-sub_key1: value1-1
-sub_key2: value1-2
-
-sub_key1: value2-1
-sub_key2: value2-2
-
-//@block
-
-the first line of block
-the second line of block
-
-the second paragraph in block
-
-CONFIG
-
-expected_result = <<RESULT
-a test string with tags (value1 and value2) in it
-
-the value of sub_key1 is value1-1
-the value of sub_key2 is value1-2
-
-the value of sub_key1 is value2-1
-the value of sub_key2 is value2-2
-
-the first line of block
-the second line of block
-
-the second paragraph in block
-
-RESULT
       tree = AdHocTemplate::Parser.parse(template)
       config = AdHocTemplate::RecordReader.read_record(config_data)
       tag_formatter = AdHocTemplate::DefaultTagFormatter.new
