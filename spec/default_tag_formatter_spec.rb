a2ca8ea6605a6c14a712e0b910b2766484571b05
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-09-08 11:11:15 +0900

    add tests for predefined format labels of DefaultTagFormatter

--- spec/default_tag_formatter_spec.rb
+++ spec/default_tag_formatter_spec.rb
@@ -5,6 +5,24 @@ require 'ad_hoc_template'
 
 describe AdHocTemplate do
   describe AdHocTemplate::DefaultTagFormatter do
+    before do
+      @record = {
+        'var1' => '<value1>',
+        'var2' => 'value2'
+      }
+    end
+
+    it 'has "=" format label for the default format' do
+      formatter = AdHocTemplate::DefaultTagFormatter.new
+      default = formatter.format('=', 'var1', @record)
+      expect(default).to eq('<value1>')
+    end
+
+    it 'has "h" format label for HTML encoding' do
+      formatter = AdHocTemplate::DefaultTagFormatter.new
+      default = formatter.format('h', 'var1', @record)
+      expect(default).to eq('&lt;value1&gt;')
+    end
   end
 end
 
