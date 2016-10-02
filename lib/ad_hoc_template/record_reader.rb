dd4a35ebd9a79d97bf43f6ffbf2412b0f6b0a547
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-10-02 16:05:10 +0900

    refactoring of CSVReadercsv_compatible_format?

--- lib/ad_hoc_template/record_reader.rb
+++ lib/ad_hoc_template/record_reader.rb
@@ -130,7 +130,7 @@ module AdHocTemplate
       end
 
       def self.csv_compatible_format?(data)
-        iteration_blocks_count = data.values.select {|v| v.kind_of? Array }.size
+        iteration_blocks_count = data.values.count {|v| v.kind_of? Array }
         iteration_blocks_count == 0 or (iteration_blocks_count == 1 && data.size == 1)
       end
 
