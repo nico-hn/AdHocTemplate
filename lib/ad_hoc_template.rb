589c19e67dcad17e0ba09bc29f32ad1821ae1af6
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-04-21 01:10:52 +0900

    replace self_tag_formatter with inner_label.tag_formatter in InnerLabel#visit_with_sub_record

--- lib/ad_hoc_template.rb
+++ lib/ad_hoc_template.rb
@@ -67,7 +67,7 @@ module AdHocTemplate
 
       prepare_sub_records(iteration_tag_node, inner_label).map do |record|
         if tag_node.contains_any_value_assigned_tag_node?(record)
-          visit_with_sub_record(tag_node, record, memo, inner_label.tag_formatter)
+          visit_with_sub_record(tag_node, record, memo, inner_label)
         elsif fallback_nodes = select_fallback_nodes(tag_node)
           format_fallback_tags(fallback_nodes, record, memo, inner_label.tag_formatter)
         else
@@ -114,8 +114,8 @@ module AdHocTemplate
       new_record || record
     end
 
-    def visit_with_sub_record(tag_node, record, memo, self_tag_formatter)
-      data_loader = AdHocTemplate::DataLoader.new(record, self_tag_formatter)
+    def visit_with_sub_record(tag_node, record, memo, inner_label)
+      data_loader = AdHocTemplate::DataLoader.new(record, inner_label.tag_formatter)
       tag_node.map {|leaf| leaf.accept(data_loader, memo) }.join
     end
 
